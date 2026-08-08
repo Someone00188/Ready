e40f9352e92e0d62c336d106ec0043fca1c122d3
Someone00188 <xayrullah013@gmail.com>
2026-08-08 20:54:37 +0500

    Fix formatting issue in App.jsx
    hwhj

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect, useCallback } from 'react';
 import { BrowserRouter, Routes, Route, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import { useTelegram } from './hooks/useTelegram';
